6373eefadd9afa2164c53c2d045d65e6effaa348
Alana Abreu <149194023+N1nh4@users.noreply.github.com>
2025-05-29 15:02:22 -0300

    roteiro7-parte1

---
+++ src/roteiro7/parte1/BancoCaixa.java
@@ -0,0 +1,34 @@
+package roteiro7.parte1;
+
+public class BancoCaixa {
+    public BancoCaixa() {
+        // Construtor vazio
+    }
+
+    public Boleto gerarBoleto(int vencimento, double valor) {
+        Boleto boleto = null;
+
+        switch (vencimento) {
+            case 10:
+                boleto = new BoletoCaixa10Dias(valor);
+                break;
+            case 30:
+                boleto = new BoletoCaixa30Dias(valor);
+                break;
+            case 60:
+                boleto = new BoletoCaixa60Dias(valor);
+                break;
+            default:
+                throw new UnsupportedOperationException("ERRO: Vencimento indisponível.");
+        }
+
+        System.out.println("*********************");
+        System.out.println("Boleto gerado com sucesso. Valor = " + valor);
+        System.out.println("Valor Juros = " + boleto.calcJurus());
+        System.out.println("Valor Desconto = " + boleto.calcDesconto());
+        System.out.println("Valor Multa = " + boleto.calcMulta());
+        System.out.println("*********************");
+
+        return boleto; 
+    }
+}
